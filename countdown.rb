5fca2ec701ca44393ae3ce9f0576af171ee9400d
Adam Workman <adammworkman25@gmail.com>
2020-06-16 22:17:44 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,10 @@
 #write your code here
 
-def countdown(10)
-   
-      until countdown < 0 
-        puts "#{countdown} SECOND(S)!"
-          countdown -= 1 
+def countdown(number)
+   number = 10 
+     while number > 0 
+        puts "#{number} SECOND(S)!"
+          number -= 1 
       end
  "HAPPY NEW YEAR!"
 end
